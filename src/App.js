34c546314a13d124d40c7e5e21092a89fe8d0b30
joshmickyle <josh.mickyle@gmail.com>
2020-07-07 16:32:39 +0200

    Deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './CSS/normalize.css';
+import './CSS/styles.css';
+function App() {
+  return (
+    <div className="App">
+      My app
+    </div>
+  );
+}
+
+export default App;
